1e091694e7ed44d7094a6224d8a4094f84f3d976
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2026-01-28 16:52:50 +0200

    update_PARM

--- ESP32/parameters.h
+++ ESP32/parameters.h
@@ -1,27 +1,54 @@
-//user defined parameters
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
 
-// OLED display width and height, in pixels
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+#include <Arduino.h> 
+// ===================== הגדרות כלליות =====================
+const char* PATIENT_ID = "p1";
+const char* CLOUD_URL  = "https://receivedata-ddy3ss2dzq-uc.a.run.app";
+const unsigned long WIFI_RETRY_MS = 30000;
 
-//  4x3 keypad GPIO pins
-#define R4   19 
-#define R3   13 
-#define R2   12 
-#define R1   4    
-#define C1   21 
-#define C2   27 
-#define C3   33  
+// ===================== הגדרות רדאר (Receiver) =====================
+#define RX_PIN 26
+#define TX_PIN 27
 
-// INMP441 I2S microphone GPIO pins
-#define I2S_WS 14
-#define I2S_SD 15
-#define I2S_SCK 32
-#define I2S_PORT I2S_NUM_0
+const unsigned long IMU_FRESH_MS = 2000;
+const float GATE_DPOS_M = 0.8f;
+const float GATE_DV     = 0.7f;
+const float V_MOVE_TH   = 0.12f;
+const float FAST_V      = 0.85f;
+const int NEED_STABLE_TO_LOCK = 6;
+const int MAX_LOST_FRAMES     = 40;
+const float MIN_Y_M = 0.3f;
+const float MAX_Y_M = 4.0f;
 
-// external DAC MAX98357A GPIO pins
-#define DAC_BCK_PIN 26
-#define DAC_WS_PIN 25
-#define DAC_DATA_PIN 22
+const unsigned long IMMOBILITY_TIMEOUT = 30000;
 
+// =====================  (IMU Sender) =====================
+#define MPU9250_ADDR 0x68
+const uint8_t RADAR_MAC_ADDR[6] = {0xCC, 0xDB, 0xA7, 0x5A, 0x7F, 0xC0}; // שונה השם למניעת התנגשות
+const unsigned long ESPNOW_SEND_MS = 50;
 
+const float STEP_THRESHOLD = 0.15;
+const int BUFFER_LENGTH = 50;
+const unsigned long DEBOUNCE_DELAY = 300;
+const float Y_THRESHOLD = 0.15;
+
+const unsigned long STAIR_WIN_MS = 350;
+const float STAIRS_ALIM_RMS_TH   = 0.34f;
+const float STAIRS_TILT_RANGE_TH = 1.3f;
+const float STAIRS_DIR_RATIO = 1.25f;
+const bool INVERT_Y = false;
+
+const float IMPACT_G = 2.0f;
+const float ROT_DPS = 250.0f;
+const float TILT_DEG = 60.0f;
+const float STILL_W = 120.0f;
+const float STILL_AL = 0.2f;
+const unsigned long STILL_TIME_MS = 1000;
+const unsigned long SUSPECT_TIMEOUT_MS = 3000;
+const unsigned long OK_TIMEOUT_MS = 30000;
+const unsigned long IM_OK_HOLD_MS = 5000;
+
+const float ALPHA_G = 0.9696970f;
+
+#endif
