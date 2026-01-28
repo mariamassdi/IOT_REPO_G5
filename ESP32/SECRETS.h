53e180fab8a6b341a6e94b85135d1ae64bc5174d
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2026-01-28 17:41:50 +0200

    Add SECRETS_template.h for configuration

--- ESP32/SECRETS.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char* WIFI_SSID     = "Mariamm";    
-const char* WIFI_PASSWORD = "ma200333";
-
-#endif
---
+++ ESP32/SECRETS_template.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID     = "";    
+const char* WIFI_PASSWORD = "";
+
+
+#endif
